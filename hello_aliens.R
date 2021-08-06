0dfd55e05817b42a8255ac2accfd58ca47f7098e
Miguel Benitez <mbenitez@cmu.edu>
2021-08-05 20:42:57 -0400

    project file

---
+++ hello_aliens.R
@@ -0,0 +1,4 @@
+cat("Hello, \n My name is Miguel Benitez and I come in peace. Soon you will
+    \n receive our newest version of the Voyager Golden Record it will contain
+    \n information about  who we are and hopefully create a way for us to 
+    \n communicate and locate us. \n On behalf of Human Kind, \n Miguel Benitez")
